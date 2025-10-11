cd19d2cea29a00dbc3fe86625dffc8d9d5b88fe8
moqee <moqeetshahid@gmail.com>
2025-10-11 19:43:06 +0100

    Added necessary startup files

--- accounts/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
+++ accounts/accounts/src/main/java/com/eazybytes/accounts/controller/AccountsController.java
@@ -1,8 +1,7 @@
-package controller;
+package com.eazybytes.accounts.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,8 +9,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class AccountsController {
 
     @GetMapping("/hello")
-    public String sayHello(@RequestParam String name){
-        return "Hello " + name;
+    public String sayHello(){
+        return "Hello World";
     }
 
 }
--- accounts/accounts/src/test/java/controller/AccountsControllerTest.java
+++ accounts/accounts/src/test/java/controller/AccountsControllerTest.java
@@ -1,15 +1,13 @@
 package controller;
 
+import com.eazybytes.accounts.controller.AccountsController;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
 
 @WebMvcTest(AccountsController.class)
 @ContextConfiguration(classes = AccountsController.class)
@@ -21,10 +19,9 @@ class AccountsControllerTest {
     @Test
     public void getHelloTest() throws Exception {
 
-        mockMvc.perform(get("/api/hello")
-                        .param("name","Moqeet"))
+        mockMvc.perform(get("/api/hello"))
                 .andExpectAll(
-                content().string("Hello Moqeet"));
+                content().string("Hello World"));
 
     }
 
