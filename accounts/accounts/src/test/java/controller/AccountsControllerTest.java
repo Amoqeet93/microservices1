9461e58618d86a15fae7d22a80c315ae1a2921ab
moqee <moqeetshahid@gmail.com>
2025-10-12 11:36:09 +0100

    Day 2 - Added files and made changes | All tests passed

--- accounts/accounts/src/test/java/controller/AccountsControllerTest.java
+++ accounts/accounts/src/test/java/controller/AccountsControllerTest.java
@@ -1,13 +1,17 @@
 package controller;
 
 import com.eazybytes.accounts.controller.AccountsController;
+import com.eazybytes.accounts.dto.CustomerDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(AccountsController.class)
 @ContextConfiguration(classes = AccountsController.class)
@@ -15,6 +19,9 @@ class AccountsControllerTest {
 
     @Autowired
     MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
 
     @Test
     public void getHelloTest() throws Exception {
@@ -24,5 +31,43 @@ class AccountsControllerTest {
                 content().string("Hello World"));
 
     }
+
+    @Test
+    public void shouldCreateAccount() throws Exception {
+
+        CustomerDto customerDto = new CustomerDto("Moqeet", "moqeet@example.com", "01212");
+
+        mockMvc.perform(post("/api/create")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(customerDto))
+        ).andExpectAll(
+                status().isCreated(),
+                jsonPath("$.statusCode").value("201"),
+                jsonPath("$.statusMsg").value("Account created successfully")
+        );
+    }
+
+//    @Test
+//    public void shouldReturnListOfAccounts() throws Exception{
+//
+//        //Does the list not need to be entered into the database first?
+//        //We are expecting a list of results back
+//
+//        //Given
+//        List<CustomerDto> customers = List.of(
+//                new CustomerDto("Mariam", "mariam@example.com", "0121"),
+//                new CustomerDto("Ahsen", "ahsen@example.com", "0111"),
+//                new CustomerDto("Ali", "ali@example.com", "0222"),
+//                new CustomerDto("Noor", "noor@example.com", "0333")
+//        );
+//
+//        //When
+//        when(accountServiceImpl.findAllAccounts()).thenReturn(customers);
+//
+//        mockMvc.perform(get("/api/accounts"))
+//                .andExpectAll(
+//                        status().isOk()
+//        );
+//    }
 
 }
